0e36d8d2d4bbbecaaf40e2308f5939a9f179fb9b
Amit Kumar <toamitkumar@gmail.com>
2012-12-18 12:59:22 +0530

    Functional AccordionUIView using gem AccordionView

--- Gemfile
+++ Gemfile
@@ -1,4 +1,6 @@
 # A sample Gemfile
 source "https://rubygems.org"
 
+gem 'rake'
 gem 'bubble-wrap'
+gem 'accordion_view'
--- app/view_controller.rb
+++ app/view_controller.rb
@@ -6,53 +6,32 @@ class ViewController < UIViewController
     label.textAlignment = UITextAlignmentCenter
     self.view.addSubview(label)
 
-    @accordion = AccordionView.alloc.initWithFrame([[5,75], [self.view.frame.size.width-10, 250]])
+    @accordion = Accordion.alloc.initWithFrame([[5,75], [self.view.frame.size.width-10, 250]])
     self.view.addSubview(@accordion)
 
-    header1 = UIButton.alloc.initWithFrame(CGRectMake(0, 0, 0, 30))
-    header1.setTitle("Section 1", forState:UIControlStateNormal)
-    header1.setTitleColor(UIColor.whiteColor, forState:UIControlStateNormal)
-    header1.backgroundColor = "#808080".to_color
-    header1.layer.borderWidth = 1
-    header1.layer.borderColor = "#dedfe1".to_color.CGColor
-
-    view1 = UIView.alloc.initWithFrame(CGRectMake(0, 0, 0, 100))
-    view1.backgroundColor = "#C0C0C0".to_color
-    view1.layer.borderWidth = 1
-    view1.layer.borderColor = "#dedfe1".to_color.CGColor
-
-    @accordion.addHeader(header1, withView:view1)
-
-    header2 = UIButton.alloc.initWithFrame(CGRectMake(0, 0, 0, 30))
-    header2.setTitle("Section 2", forState:UIControlStateNormal)
-    header2.backgroundColor = "#808080".to_color
-    header2.setTitleColor(UIColor.whiteColor, forState:UIControlStateNormal)
-    header2.layer.borderWidth = 1
-    header2.layer.borderColor = "#dedfe1".to_color.CGColor
-
-    view2 = UIView.alloc.initWithFrame(CGRectMake(0, 0, 0, 100))
-    view2.backgroundColor = "#C0C0C0".to_color
-    view2.layer.borderWidth = 1
-    view2.layer.borderColor = "#dedfe1".to_color.CGColor
-
-    @accordion.addHeader(header2, withView:view2)
-
-    header3 = UIButton.alloc.initWithFrame(CGRectMake(0, 0, 0, 30))
-    header3.setTitle("Section 3", forState:UIControlStateNormal)
-    header3.backgroundColor = "#808080".to_color
-    header3.setTitleColor(UIColor.whiteColor, forState:UIControlStateNormal)
-    header3.layer.borderWidth = 1
-    header3.layer.borderColor = "#dedfe1".to_color.CGColor
-
-    view3 = UIView.alloc.initWithFrame(CGRectMake(0, 0, 0, 100))
-    view3.backgroundColor = "#C0C0C0".to_color
-    view3.layer.borderWidth = 1
-    view3.layer.borderColor = "#dedfe1".to_color.CGColor
-
-    @accordion.addHeader(header3, withView:view3)
-
-    @accordion.setSelectedIndex(-1)
+    add_accordion("Section 1")
+    add_accordion("Section 2")
+    add_accordion("Section 3")
+    
+    @accordion.set_selected_index(-1)
     @accordion.setNeedsLayout
+  end
+
+  private 
+  def add_accordion(header_name)
+    header = UIButton.alloc.initWithFrame(CGRectMake(0, 0, 0, 30))
+    header.setTitle(header_name, forState:UIControlStateNormal)
+    header.setTitleColor(UIColor.whiteColor, forState:UIControlStateNormal)
+    header.backgroundColor = "#808080".to_color
+    header.layer.borderWidth = 1
+    header.layer.borderColor = "#dedfe1".to_color.CGColor
+
+    view = UIView.alloc.initWithFrame(CGRectMake(0, 0, 0, 100))
+    view.backgroundColor = "#C0C0C0".to_color
+    view.layer.borderWidth = 1
+    view.layer.borderColor = "#dedfe1".to_color.CGColor
+
+    @accordion.add_header(header, withView:view)
   end
 
 end
